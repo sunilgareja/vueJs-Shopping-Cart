d8d5f2c82bbb789b2b3acbd062b1ca1ff9d2a5e3
SunnieG <sunil_gareja@hotmail.co.uk>
2019-07-06 15:17:09 +0100

    source files

---
+++ data.js
@@ -0,0 +1,67 @@
+var inventory = [{
+  'name': 'USB Stick',
+
+  'options': [{
+      'id': 1,
+      'details': '16GB',
+      'price': 6.95
+  }, {
+      'id': 2,
+      'details': '32GB',
+      'price': 10
+  }, {
+      'id': 3,
+      'details': '64GB',
+      'price': 25.95
+  }]
+}, {
+  'name': 'USB Plug',
+
+  'options': [{
+      'id': 4,
+      'price': 4.35,
+      'details': '3ft cable'
+  }, {
+      'id': 5,
+      'price': 9,
+      'details': '6ft cable'
+  }]
+}, {
+  'name': 'Smart Phone',
+
+  'options': [{
+      'id': 6,
+      'price': 799,
+      'details': 'Samsung Phone'
+  }, {
+      'id': 7,
+      'price': 1000,
+      'details': 'IPhone'
+  }, {
+      'id': 8,
+      'price': 609,
+      'details': 'Google Phone'
+  }
+
+]
+}, {
+  'name': 'Camera',
+
+  'options': [{
+      'id': 9,
+      'price': 76.50,
+      'details': 'Blue Camera'
+  }, {
+      'id': 10,
+      'price': 86.50,
+      'details': 'Red Camera'
+  }, {
+      'id': 11,
+      'price': 55.50,
+      'details': 'Yellow Camera'
+  }, {
+      'id': 12,
+      'price': 45.50,
+      'details': 'Purple Camera'
+  }]
+}];
